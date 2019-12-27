923e442de77a37529b4948c2a26d520c29a624d4
chris <cdepauw@iconicengine.com>
2019-12-27 14:34:33 -0800

    small update to unit tests -- two tests only now

--- test/react-dropdown-now.spec.js
+++ test/react-dropdown-now.spec.js
@@ -1,13 +1,46 @@
 import test from 'ava';
 import React from 'react';
+import sinon from 'sinon';
 import { mount } from 'enzyme';
 
 import ReactDropdownNow from '../index';
 
 test('ReactDropdownNow, opens', t => {
   const component = mount(
-    <ReactDropdownNow onClick={() => {}} />
+    <ReactDropdownNow
+      options={[ 'one', 'two', 'three' ]}
+      value={'one'}
+    />
   );
 
-  t.true(/div/.test(component.html()));
+  component
+    .find('.Dropdown-control')
+    .simulate('mousedown', { button: 0 });
+
+  t.true(/class="Dropdown-root is-open"/.test(component.html()));
+  component.unmount();
+});
+
+test('ReactDropdownNow, calls onChange', t => {
+  const onChange = sinon.spy();
+  const component = mount(
+    <ReactDropdownNow
+      options={[ 'one', 'two', 'three' ]}
+      value={'one'}
+      onChange={onChange}
+    />
+  );
+
+  component
+    .find('.Dropdown-control')
+    .simulate('mousedown', { button: 0 });
+
+  component
+    .find('.Dropdown-option')
+    .at(0)
+    .simulate('mousedown', { button: 0 });
+
+  t.true(onChange.calledOnce);
+  t.false(/class="Dropdown-root is-open"/.test(component.html()));
+  component.unmount();
 });
