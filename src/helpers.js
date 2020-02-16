5644227de8a4794638e79638c70148018394dfde
chris <chris@bumblehead.com>
2020-02-15 23:07:13 -0800

    update helper functions per review

--- src/helpers.js
+++ src/helpers.js
@@ -2,19 +2,25 @@ export const isValidLabelOrValue = value => (
   /string|boolean|number/.test(typeof value));
 
 export const getOptionLabel = (option, label = option) => {
-  if (isValidLabelOrValue(option.label))
-    label = option.label;
-  else if (isValidLabelOrValue(option.value))
-    label = option.value;
+  if (isValidLabelOrValue(option.label)) {
+    return option.label;
+  }
+
+  if (isValidLabelOrValue(option.value)) {
+    return option.value;
+  }
 
   return label;
 };
 
 export const getOptionValue = (option, value = option) => {
-  if (isValidLabelOrValue(option.value))
-    value = option.value;
-  else if (isValidLabelOrValue(option.label))
-    value = option.label;
+  if (isValidLabelOrValue(option.value)) {
+    return option.value;
+  }
+
+  if (isValidLabelOrValue(option.label)) {
+    return option.label;
+  }
 
   return value;
 };
