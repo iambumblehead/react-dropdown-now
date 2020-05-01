a0f4107571d428053a5f3a19faa5f67bb9a2b5cd
Layton Whiteley <layton.whiteley@gmail.com>
2020-05-01 08:26:04 +0200

    chore: update typescipt definition

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,32 @@
 declare module 'react-dropdown-now' {
   import * as React from 'react';
+  export enum RenderType {
+    OPTION,
+    LABEL,
+  }
   export interface Option {
-    label: React.ReactNode;
-    view: React.ReactNode;
     value: string;
+    label: React.ReactNode;
+    view?: React.ReactNode;
+    id?: string;
     className?: string;
   }
+  export interface MatchItem {
+    option: Option;
+    id: string;
+    index: number;
+    type: RenderType;
+  }
   export interface Group {
-    type: 'group';
     name: string;
     items: Option[];
   }
 
   type StringOrReactNode = string | React.ReactNode;
+  type Value = Option | string;
 
   export interface ReactDropdownProps {
-    options: (Group | Option | string)[];
+    options: (Group | Option | string | number)[];
     baseClassName?: string;
     className?: string;
     controlClassName?: string;
@@ -26,11 +37,12 @@ declare module 'react-dropdown-now' {
     disabled?: boolean;
     arrowClosed?: React.ReactNode;
     arrowOpen?: React.ReactNode;
-    onChange?: (arg: Option) => void;
+    onChange?: (arg: Option, event: React.SyntheticEvent) => void;
     onFocus?: (arg: boolean) => void;
     onOpen?: () => void;
     onClose?: (closedBySelection: boolean) => void;
-    value?: Option | string;
+    matcher?: (item: MatchItem, value: Value) => void;
+    value?: Value;
     placeholder?: String;
     innerRef: React.Ref;
   }
